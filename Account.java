f141d30f3b7e3ee84a1443b7e2e9b093ea557454
Sarah Dimovski <sarahmdimovski@gmail.com>
2023-11-14 15:49:29 +0100

    Gamle account system

--- Account.java
+++ Account.java
@@ -1,3 +1,32 @@
+package Main;
+
 public class Account {
+    private int balance;
+    Dicevalues roll = new Dicevalues();
+    FieldEffects fieldEffects = new FieldEffects();
+
+    public Account(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int rollResult, int effectAmount, int specialFieldResult) {
+        int newBalance = balance + effectAmount;
+        balance = newBalance;
+    }
+
+    public void withdraw(int rollResult, int effectAmount, int specialFieldResult) {
+        int newBalance = balance;
+        if (rollResult == 10) {
+            int extraRollResult = roll.rollDice();
+            int extraEffectAmount = fieldEffects.getEffect(extraRollResult);
+            newBalance += extraEffectAmount;
+        }
+        newBalance -= effectAmount;
+        balance = Math.max(0, newBalance);
+    }
     
 }

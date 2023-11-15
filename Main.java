8e5e5386409933c04a6ce96d8f954199e5143ac7
Sarah Dimovski <sarahmdimovski@gmail.com>
2023-11-15 12:43:45 +0100

    Initialize game with players selected by user

--- Main.java
+++ Main.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+   List<Player> players = new ArrayList<>();
+    Dicevalues roll = new Dicevalues(); //Initialize the dice roll
     
+    Scanner s = new Scanner(System.in); 
+
+    System.out.println("Hello and Welcome to the Monopoly Game Junior! Enter the number of players: ");
+
+    int numPlayer = s.nextInt(); 
+
+    if (numPlayers >= 2 && numPlayers <= 4) {
+
+        for(int i = 1; i <= numPlayers; i++ ){
+            players.add(new Player("Player " + i));
+        }
+
+    }
+}
 }

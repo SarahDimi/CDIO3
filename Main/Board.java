3437ceeaf453d55039807409ec7143bbd55e1a39
s235080 <nikoplund@gmail.com>
2023-11-17 13:00:05 +0100

    Added text description to Board & SpecialField

--- Main/Board.java
+++ Main/Board.java
@@ -18,31 +18,34 @@ public class Board {
         initializeBoard();
     }
 
+    // Each Field is placed in correct order according to its place on the board. Name, Prices and Rent shown.
+
     private void initializeBoard() {
-        // Add your fields to the board
-        specialfield.add(new SpecialField.StartField());
-        fields.add(new Field("Istergade number 0",1,1));
-        fields.add(new Field("DTU",1,1));
-        chancecard.add(new ChanceCard());
-        fields.add(new Field("Blaagaards Plads",1,1));
-        fields.add(new Field("Shamba",1,1));
-        specialfield.add(new SpecialField.VisitJail());
-        fields.add(new Field("Alis kebabshop",2,2));
-        fields.add(new Field("Noerrebros Runddel",2,2));
-        chancecard.add(new ChanceCard());
-        fields.add(new Field("Amager",2,2));
-        fields.add(new Field("Vesterbro",2,2));
-        specialfield.add(new SpecialField.FreeParking());
-        fields.add(new Field("Holte",3,3));
-        fields.add(new Field("Gammel Holte",3,3));
-        chancecard.add(new ChanceCard());
-        fields.add(new Field("Campus bar",3,3));
-        fields.add(new Field("Oesterbro",3,3));
-        specialfield.add(new SpecialField.InJail());
-        fields.add(new Field("Frederiksberg",4,4));
-        fields.add(new Field("The Zoo",4,4));
-        chancecard.add(new ChanceCard());
-        fields.add(new Field("Strandvejen",5,5));
-        fields.add(new Field("Hellerup",5,5));
+
+        specialfield.add(new SpecialField.StartField());    //Start field
+        fields.add(new Field("Istergade number 0",1,1));    //Brown field 1
+        fields.add(new Field("DTU",1,1));    //Brown fields 2
+        chancecard.add(new ChanceCard());    //Chance field 1
+        fields.add(new Field("Blaagaards Plads",1,1));  //Light blue field 1
+        fields.add(new Field("Shamba",1,1));    //Light blue field 2
+        specialfield.add(new SpecialField.VisitJail());     //Visiting Jail field
+        fields.add(new Field("Alis kebabshop",2,2));     //Pink field 1
+        fields.add(new Field("Noerrebros Runddel",2,2));     //Pink field 2
+        chancecard.add(new ChanceCard());    //Chance field 2
+        fields.add(new Field("Amager",2,2));    //Orange field 1
+        fields.add(new Field("Vesterbro",2,2));     //Orange field 2
+        specialfield.add(new SpecialField.FreeParking());     //Free parking field 
+        fields.add(new Field("Holte",3,3));     //Red Field 1
+        fields.add(new Field("Gammel Holte",3,3));     //Red Field 2
+        chancecard.add(new ChanceCard());    //Chance field 3
+        fields.add(new Field("Campus bar",3,3));     //Yellow field 1
+        fields.add(new Field("Oesterbro",3,3));     //Yellow field 2
+        specialfield.add(new SpecialField.InJail());     //In Jail field
+        fields.add(new Field("Frederiksberg",4,4));     //Green Field 1
+        fields.add(new Field("The Zoo",4,4));      //Green Field 2
+        chancecard.add(new ChanceCard());      //Chance field 4
+        fields.add(new Field("Strandvejen",5,5));    //Dark blue field 1
+        fields.add(new Field("Hellerup",5,5));      //Dark blue field 2
+   
     }
 }

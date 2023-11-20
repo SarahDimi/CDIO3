86cb2486872065525799cd6a768d47e9a7168f15
Vittrup02 <tobias2311@gmail.com>
2023-11-20 14:07:11 +0100

    Major changes to ChanceCard

--- Main/ChanceCard.java
+++ Main/ChanceCard.java
@@ -2,6 +2,8 @@ package Main;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+
 class ChanceCard {
     private List<String> cards;
 
@@ -15,12 +17,10 @@ class ChanceCard {
         cards.add("Free field! Move to a ORANGE or GREEN field. If the field is available, you get it for free otherwise you must pay rent to the owner");
         cards.add("Free field! Move to a LIGHT BLUE field. If the field is available, you get it for free otherwise you must pay rent to the owner");
         cards.add("You can get out of jail. Keep this card until you need it");
-        cards.add("Move to the beach"); // beach can be changed to antoher field
         cards.add("It is you birthdday!! All the other player must give you $1. Congratulations!!");
         cards.add("Free field! Move to a PINK or DARK BLUE field. If the field is available, you get it for free otherwise you must pay rent to the owner");
         cards.add("You have made all your homework. You will get $2 form the bank");
         cards.add("Free field! Move to a RED field. If the field is available, you get it for free otherwise you must pay rent to the owner");
-        cards.add("Free field! Move to the SKATEPARK field. If the field is available, you get it for free otherwise you must pay rent to the owner");
         cards.add("Free field! Move to a LIGHT BLUE or RED field. If the field is available, you get it for free otherwise you must pay rent to the owner");
         cards.add("Free field! Move to a BROWN or YELLOW field. If the field is available, you get it for free otherwise you must pay rent to the owner");
        
@@ -29,72 +29,264 @@ class ChanceCard {
     public String drawCard() {
         int randomIndex = (int)(Math.random() * cards.size());
         String message = cards.get(randomIndex);
-        cards.remove(randomIndex); // Remove the drawn card from the deck
+        cards.remove(randomIndex); 
         return message;
     }
     public void applyCardEffect(Player player, String cardMessage, PlayerPiece playerPiece, int currentPlayerPosition) {
+        Scanner scanner = new Scanner(System.in);
         if (cardMessage.equals("Advance to Go (Collect $2)")) {
-            // Also need to move the player to Go
             player.getAccount().deposit(2);
             System.out.println(player.getName() + " Advances to Go and collects $2");
+
+            int startFieldIndex = 0; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (startFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+
         } else if (cardMessage.equals("Move up to 5 fields forward")) {
             Random random = new Random();
-            int spacesToMove = random.nextInt(6); // Generates a random number between 0 and 5 (inclusive)
+
+            int spacesToMove = random.nextInt(6); 
             playerPiece.moveForward(spacesToMove);
             System.out.println(player.getName() + " moves " + spacesToMove + " spaces forward.");
+
         } else if (cardMessage.equals("Free field! Move to an ORANGE field. If available, you get it for free, otherwise pay rent.")) {
-            // Determine the target field index for the ORANGE field
-            int orangeFieldIndex = // Set the index for the ORANGE field;
+            int orangeFieldIndex = 10; 
         
-            // Get the current position of the player
-            int currentPlayerPosition = playerPiece.getPosition();
+            currentPlayerPosition = playerPiece.getPosition();
         
-            // Calculate the number of spaces to move to reach the ORANGE field
-            int spacesToMove = (orangeFieldIndex - currentPlayerPosition + Board.fields.size()) % Board.fields.size();
+            int spacesToMove = (orangeFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
         
-            // Move the player to the ORANGE field
             playerPiece.moveForward(spacesToMove);
         
-            // Check if the ORANGE field is available
             Player owner = Bank.getFieldOwner(orangeFieldIndex);
             if (owner == null) {
-                // The field is available, the player gets it for free
                 Bank.fieldOwnership.put(orangeFieldIndex, player);
                 System.out.println(player.getName() + " acquires the ORANGE field for free.");
             } else {
-                // The field is owned by another player, pay rent
-                int rentAmount = // Set the rent amount for the ORANGE field;
+                int rentAmount = 2; 
                 Bank.payRent(player, owner, rentAmount);
                 System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the ORANGE field.");
             }
         } else if (cardMessage.equals("Move one field forward or draw another Chance card")) {
-            // Implement the logic to move the player forward by one field or draw another chance card
+            String answer = scanner.nextLine();
+
+            if(answer == "1"){
+                playerPiece.moveForward(1);
+            } else {
+                drawCard();
+            }
         } else if (cardMessage.equals("You have eaten too much candy, pay $2 to the bank")) {
             player.getAccount().deposit(2);
             System.out.println(player.getName() + " pays $2 to the bank for eating to much candy");
         } else if (cardMessage.equals("Free field! Move to an ORANGE or GREEN field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to an ORANGE or GREEN field and handling property transactions
+
+            System.out.println("You can now choose between a ORANGE field or a GREEN field. Write which you choose in the");
+            String fieldAnswer = scanner.nextLine();
+
+            if(fieldAnswer.equalsIgnoreCase ("orange")){
+            int orangeFieldIndex = 10; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (orangeFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(orangeFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(orangeFieldIndex, player);
+                System.out.println(player.getName() + " acquires the ORANGE field for free.");
+            } else {
+                int rentAmount = 2; 
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the ORANGE field.");
+            }
+        } else {
+            int greenFieldIndex = 19; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (greenFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(greenFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(greenFieldIndex, player);
+                System.out.println(player.getName() + " acquires the GREEN field for free.");
+            } else {
+                int rentAmount = 4; 
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the GREEN field.");
+            }
+        }
         } else if (cardMessage.equals("Free field! Move to a LIGHT BLUE field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to a LIGHT BLUE field and handling property transactions
+            int lightBlueFieldIndex = 4; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (lightBlueFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(lightBlueFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(lightBlueFieldIndex, player);
+                System.out.println(player.getName() + " acquires the ORANGE field for free.");
+            } else {
+                int rentAmount = 1; 
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the ORANGE field.");
+            }
         } else if (cardMessage.equals("You can get out of jail. Keep this card until you need it")) {
             // Implement logic to keep the card for later use when the player is in jail
-        } else if (cardMessage.equals("Move to the beach")) {
-            // Implement logic to move the player to the beach or any other specified field
         } else if (cardMessage.equals("It is your birthday!! All other players must give you $1. Congratulations!!")) {
             // Implement logic to collect $1 from all other players
         } else if (cardMessage.equals("Free field! Move to a PINK or DARK BLUE field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to a PINK or DARK BLUE field and handling property transactions
+            System.out.println("You can now choose between a DARK BLUE field or a PINK field. Write which you choose in the");
+            String fieldAnswer = scanner.nextLine();
+            if(fieldAnswer.equalsIgnoreCase ("dark blue")){
+            int darkBlueFieldIndex = 22; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (darkBlueFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(darkBlueFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(darkBlueFieldIndex, player);
+                System.out.println(player.getName() + " acquires the DARK BLUE field for free.");
+            } else {
+                int rentAmount = 5; 
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the DARK BLUE field.");
+            }
+        } else {
+            int pinkFieldIndex = 7; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (pinkFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(pinkFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(pinkFieldIndex, player);
+                System.out.println(player.getName() + " acquires the GREEN field for free.");
+            } else {
+                int rentAmount = 2;
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the GREEN field.");
+            }
+        }
         } else if (cardMessage.equals("You have completed your homework. You receive $2 from the bank")) {
             player.getAccount().deposit(2);
             System.out.println(player.getName() + " gets $2 for completing all of there homwwork");
         } else if (cardMessage.equals("Free field! Move to a RED field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to a RED field and handling property transactions
-        } else if (cardMessage.equals("Free field! Move to the SKATEPARK field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to the SKATEPARK field and handling property transactions
+            int redFieldIndex = 13; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (redFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(redFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(redFieldIndex, player);
+                System.out.println(player.getName() + " acquires the ORANGE field for free.");
+            } else {
+                int rentAmount = 2;
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the ORANGE field.");
+            }
         } else if (cardMessage.equals("Free field! Move to a LIGHT BLUE or RED field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to a LIGHT BLUE or RED field and handling property transactions
+            System.out.println("You can now choose between a LIGHT BLUE field or a RED field. Write which you choose");
+            String fieldAnswer = scanner.nextLine();
+            if(fieldAnswer.equalsIgnoreCase ("light blue")){
+            int lightBlueFieldIndex = 4; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (lightBlueFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(lightBlueFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(lightBlueFieldIndex, player);
+                System.out.println(player.getName() + " acquires the DARK BLUE field for free.");
+            } else {
+                int rentAmount = 1;
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the DARK BLUE field.");
+            }
+        } else {
+            int redFieldIndex = 13; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (redFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(redFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(redFieldIndex, player);
+                System.out.println(player.getName() + " acquires the GREEN field for free.");
+            } else {
+                int rentAmount = 3;
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the GREEN field.");
+            }
+        }
         } else if (cardMessage.equals("Free field! Move to a BROWN or YELLOW field. If available, you get it for free, otherwise pay rent.")) {
-            // Implement logic for moving to a BROWN or YELLOW field and handling property transactions
+            System.out.println("You can now choose between a BROWN field or a YELLOW field. Write which you choose in the");
+            String fieldAnswer = scanner.nextLine();
+            if(fieldAnswer.equalsIgnoreCase ("brown")){
+            int brownFieldIndex = 1; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (brownFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(brownFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(brownFieldIndex, player);
+                System.out.println(player.getName() + " acquires the DARK BLUE field for free.");
+            } else {
+                int rentAmount = 1;
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the DARK BLUE field.");
+            }
+        } else {
+            int yellowFieldIndex = 16; 
+        
+            currentPlayerPosition = playerPiece.getPosition();
+        
+            int spacesToMove = (yellowFieldIndex - currentPlayerPosition + Board.Fields().size()) % Board.Fields().size();
+        
+            playerPiece.moveForward(spacesToMove);
+        
+            Player owner = Bank.getFieldOwner(yellowFieldIndex);
+            if (owner == null) {
+                Bank.fieldOwnership.put(yellowFieldIndex, player);
+                System.out.println(player.getName() + " acquires the GREEN field for free.");
+            } else {
+                int rentAmount = 3; 
+                Bank.payRent(player, owner, rentAmount);
+                System.out.println(player.getName() + " pays rent to " + owner.getName() + " for the GREEN field.");
+            }
+        }
         }
     }
 }
